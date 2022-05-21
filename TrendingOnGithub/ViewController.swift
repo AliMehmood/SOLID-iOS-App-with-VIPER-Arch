8074a6e2f2bbe268344092e46144d6237d208b1d
Ali Mehmood <ali.mehmood@avanza.pk>
2022-05-21 17:06:45 +0500

    Initial Commit

---
+++ TrendingOnGithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingOnGithub
+//
+//  Created by Mehmood on 21/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
