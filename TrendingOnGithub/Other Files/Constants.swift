dfd9eec11279b0f36cfad3698f19edb8d7f485a0
Ali Mehmood <ali.mehmood@avanza.pk>
2022-05-31 14:27:55 +0500

    Constants file

---
+++ TrendingOnGithub/Other Files/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+
+//
+//  Created by Mehmood on 16/05/2022.
+//
+
+import Foundation
+struct Constants{
+    enum URLConstants : String {
+        case reposAPI = "https://api.github.com/search/repositories?q=language=+sort:stars"
+    }
+}
