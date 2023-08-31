94bbb9415f4bcdf8d7287f359954062a87589df0
Mehmood <ali.mehmood@avanzasolutions.com>
2023-08-31 17:21:02 +0500

    request Notification Permission

--- TrendingOnGithub/Other Files/AppDelegate.swift
+++ TrendingOnGithub/Other Files/AppDelegate.swift
@@ -14,9 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.requestNotificationPermission()
         return true
     }
 
+    
+    func requestNotificationPermission(){
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if success {
+                print("Notification Permission set!")
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
